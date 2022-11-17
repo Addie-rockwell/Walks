4a70357b63965febfe9d530475db2e497229d441
Addie-rockwell <asjia@ra.rockwell.com>
2022-11-17 17:00:18 +0800

    Initialize

---
+++ NewWalks/NewWalks.API/Data/WalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NewWalks.API.Models.Domain;
+
+namespace NewWalks.API.Data
+{
+    public class WalksDbContext:DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
